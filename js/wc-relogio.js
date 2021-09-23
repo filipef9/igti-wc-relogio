379f1d93d93482fbf0971e6c8831430ff5b47cb8
filipef9 <filipef9@hotmail.com>
2021-09-23 18:43:27 +0000

    Implementação do ponteiro de horas do relógio analógico.

--- js/wc-relogio.js
+++ js/wc-relogio.js
@@ -5,7 +5,13 @@
             <div class="digital">
                 <span></span>
             </div>
-            <div class="analogico">
+            <div class="analogic">
+                <svg height="210" width="500">
+                    <circle cx="100" cy="100" r="60" fill="none" stroke="black"/>
+                    <g id="hourPointer" transform="rotate(360, 100, 100)">
+                        <path d="M 100 100 V 50" stroke="blue" />
+                    </g>
+                </svg>
             </div>
         </div>
     `;
@@ -13,16 +19,24 @@
     class WcRelogioElement extends HTMLElement {
         constructor() {
             super();
+            
             this.attachShadow({ mode: 'open' });
-
             this.shadowRoot.appendChild(template.content.cloneNode(true));
+
             this.digitalDisplay = this.shadowRoot.querySelector('.digital>span');
+            this.analogicDisplay = this.shadowRoot.querySelector('.analogic>svg');
+
+            this.hourPointer = this.shadowRoot.getElementById('hourPointer');
         }
 
         connectedCallback() {
-            this.digitalDisplay.textContent = getHMS();
+            const {h, m, s} = getHMS();
+            this.digitalDisplay.textContent = getHMSFormatted();
             this.timer = setInterval(() => {
-                this.digitalDisplay.textContent = getHMS();
+                this.digitalDisplay.textContent = getHMSFormatted();
+
+                const transform = `rotate(${h * (360 / 12)}, 100, 100)`;
+                this.hourPointer.setAttribute('transform', transform);
             });
         }
 
@@ -33,11 +47,16 @@
 
     const getHMS = () => {
         const dh = new Date();
-        const h = formatNumber(dh.getHours());
-        const m = formatNumber(dh.getMinutes());
-        const s = formatNumber(dh.getSeconds());
-        return `${h}:${m}:${s}`;
+        const h = dh.getHours();
+        const m = dh.getMinutes();
+        const s = dh.getSeconds();
+        return { h, m, s};
     };
+
+    const getHMSFormatted = () => {
+        const {h, m, s} = getHMS();
+        return `${formatNumber(h)}:${formatNumber(m)}:${formatNumber(s)}`;
+    }
 
     const formatNumber = (n) => {
         return String(n).padStart(2, '0');
